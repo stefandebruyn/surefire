28eae14f97299e8ce508707fd1629d14cb5f2763
Stefan deBruyn <stefandebruyn@utexas.edu>
2022-02-23 21:26:12 -0600

    add NO_TIME constant to Clock

--- src/sfa/pal/Clock.hpp
+++ src/sfa/pal/Clock.hpp
@@ -11,6 +11,11 @@ namespace Clock
 
     constexpr U64 MS_IN_S = 1000;
 
+    /// @brief Value reserved by the framework to represent the absence of a
+    /// time value. This is the largest time value possible at ~584 years, so it
+    /// should never be produced by the clock.
+    constexpr U64 NO_TIME = 0xFFFFFFFFFFFFFFFF;
+
     U64 nanoTime();
 
     inline void spinWait(const U64 kNs)
