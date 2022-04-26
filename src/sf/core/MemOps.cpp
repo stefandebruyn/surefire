ad3243ee9ecea6627d8599b49cfeed9aeea8271d
Stefan deBruyn <stefandebruyn@utexas.edu>
2022-04-25 22:00:27 -0500

    update watermark

--- src/sf/core/MemOps.cpp
+++ src/sf/core/MemOps.cpp
@@ -1,11 +1,18 @@
 ////////////////////////////////////////////////////////////////////////////////
 ///                             S U R E F I R E
 ///                             ---------------
-/// This file is part of Surefire, a C++ framework for building avionics
-/// software applications. Built in Austin, Texas at the University of Texas at
-/// Austin. Surefire is open-source under the Apache License 2.0 - a copy of the
-/// license may be obtained at https://www.apache.org/licenses/LICENSE-2.0.
-/// Surefire is maintained at https://www.github.com/stefandebruyn/surefire.
+/// This file is part of Surefire, a C++ framework for building flight software
+/// applications. Built in Austin, Texas at the University of Texas at Austin.
+/// Surefire is open-source under the Apache License 2.0 - a copy of the license
+/// may be obtained at https://www.apache.org/licenses/LICENSE-2.0.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+/// IN THE SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "sf/core/MemOps.hpp"
